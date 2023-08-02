ae824146f365f0cf61a4ff750cf18e3cb8a0020b
skizzle010 <raiutkarsh3219@gmail.com>
2023-08-02 23:40:30 +0530

    -navbar href left set

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Link  from 'next/link'
+import Logo  from './Logo'
+
+const CustomLink = ({href,title,className=""}) => {
+    return(
+        <Link href={href} className={`${className} relative`}>
+            {title}
+
+            <span className='h-[1px] inline-block w-full bg-dark absolute left-0 -bottom-0.5' 
+            >&nbsp;</span> 
+
+        </Link>
+    )
+}
+
+const NavBar = () => {
+  return (
+    <header className='w-full px-32 py-8 font font-medium flex items-center justify-between'>
+
+    <nav>
+        <CustomLink href='/' title= "Home" className='mr-4 '/>
+        <CustomLink href='/about' title="About" className='mx-4'/>
+        <CustomLink href='/projects' title="Projects " className='mx-4'/>
+        <CustomLink href='/contact' title= "Contact me" className='ml-4'/>
+
+    </nav>
+    <nav>
+        <Link href='/' target={'_blank'}>github</Link>
+        <Link href='/' target={'_blank'}>github</Link>
+        <Link href='/' target={'_blank'}>github</Link>
+        <Link href='/' target={'_blank'}>github</Link>
+        <Link href='/' target={'_blank'}>github</Link>
+    </nav>
+    <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo/>
+    </div>
+    </header>
+  )
+}
+
+export default NavBar
