2aade2e0e8cd2094fd183ddc5983bd79bcdb2a0f
skizzle010 <raiutkarsh3219@gmail.com>
2023-08-04 19:29:02 +0530

    feat: about respose

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,18 +5,18 @@ import LiIcon from './LiIcon';
 
 const Details = ({type,time,place,info})=>{
     const ref = useRef(null);
-   return <li ref ={ref}  className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col item-center justify-between'>
+   return <li ref ={ref}  className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col item-center justify-between md:w-[80%]'>
         <LiIcon reference ={ref}/>
         <motion.div
         initial={{y:50}}
         whileInView={{y:0}}
         transition={{duration:0.5,type:'spring'}}>
         <div>
-            <h3 className='capitalize font-bold text-2xl'>{type} </h3>
-            <span className='capitalize font-medium text-dark/75 dark:text-light/75'>
+            <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{type} </h3>
+            <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
                 {time}|{place}
             </span>
-            <p className='font-medium'>
+            <p className='font-medium md:text-sm'>
                 {info}
             </p>
         </div>
@@ -35,16 +35,18 @@ const Education = () => {
   return (
     <div className='my-64'>
 
-        <h2 className='font-bold text-8xl mb-32 w-full text-center'>
+        <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-4xl xs:3xl md:mb-16'>
             Education</h2>
 
-            <div ref={ref} className='w-[75%] mx-auto relative'>
+            <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
 
     <motion.div 
     style={{scaleY:scrollYProgress}}
-    className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light'/>
+    className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
+    md:w-[2px] md:left-[30px] xs:left-[20px]
+    '/>
         
-                <ul className='w-full flex flex-col items-start justify-between ml-4'>
+                <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
                     <Details 
                     type="Senior Secondary Education"
                     time="2018-2020" place="Bhawan Vidyalaya Pankhkula"
--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -13,7 +13,7 @@ const LiIcon = ({reference}) => {
 
     
     <figure className='absolute left-0  stroke-dark dark:stroke-light'>
-        <svg className='-rotate-90' width="75" height="75" viewBox="0 0 100 100">
+        <svg className='-rotate-90 md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px] ' width="75" height="75" viewBox="0 0 100 100">
             <circle cx="75" cy="50" r="20" className='stroke-primary stroke-1 fill-none dark:stroke-primaryDark'/>
             <motion.circle cx="75" cy="50" r="20" className='stroke-[5px] fill-light dark:fill-dark'
                 style={{pathLength:scrollYProgress}}
